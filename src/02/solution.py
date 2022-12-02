a9a6ff662c5d41df064ce754bb07a761196b91e6
Tyler Porter <tyler.b.porter@gmail.com>
2022-12-02 01:13:54 -0500

    Solution for Day 2

---
+++ src/02/solution.py
@@ -0,0 +1,13 @@
+from src.prompt import Prompt
+
+SCORE_TABLE = { 'A X': 4, 'A Y': 8, 'A Z': 3, 'B Y': 5, 'B Z': 9, 'B X': 1, 'C Z': 6, 'C X': 7, 'C Y': 2 }
+MAPPING = { 'A X': 'A Z', 'A Y': 'A X', 'A Z': 'A Y', 'B Y': 'B Y', 'B Z': 'B Z', 'B X': 'B X', 'C Z': 'C X', 'C X': 'C Y', 'C Y': 'C Z'}
+
+def part_1_solution(values):
+  return sum(SCORE_TABLE[value] for value in values)
+
+def part_2_solution(values):
+  return sum(SCORE_TABLE[MAPPING[value]] for value in values)
+
+def transform_prompt():
+  return Prompt.read(__file__)
