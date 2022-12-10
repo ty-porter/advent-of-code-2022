6bb202f1d6c1e8f861e17281a5492530b14ed098
Tyler Porter <tyler.b.porter@gmail.com>
2022-12-09 19:06:45 -0500

    Refactored solution to solve Part 2

--- src/09/solution.py
+++ src/09/solution.py
@@ -1,88 +1,24 @@
 from src.prompt import Prompt
 
-def distance(head, tail):
-  x_dist = abs(head[0] - tail[0])
-  y_dist = abs(head[1] - tail[1])
+def move(head, tail):
+  distance = [head[0] - tail[0], head[1] - tail[1]]
 
-  return [x_dist, y_dist]
+  # Already touching
+  if abs(distance[0]) <= 1 and abs(distance[1]) <= 1:
+    return
 
-def move_part(head, tail, direction):
-  dist = distance(head, tail)
+  # Diagonal
+  if sum(abs(part) for part in distance) > 2:
+    offset = [int(distance[0] / abs(distance[0])), int(distance[1] / abs(distance[1]))]
+  else:
+    offset = [int(part / abs(part)) if abs(part) > 0 else 0 for part in distance]
 
-  if dist[0] < 2 and dist[1] < 2:
-    return tail
+  tail[0] += offset[0]
+  tail[1] += offset[1]
 
-  # Diagonal, should move into rank or file H occupies
-  if sum(dist) > 2:
-    if dist[0] < dist[1]:
-      tail[0] = head[0]
-    else:
-      tail[1] = head[1]
-
-    # Recalculate distance after move
-    dist = distance(head, tail)
-
-  if direction == "U":
-    tail[1] += 1
-  elif direction == "D":
-    tail[1] -= 1
-  elif direction == "L":
-    tail[0] -= 1
-  elif direction == "R":
-    tail[0] += 1
-
-  return tail
-
-def part_1_solution(instructions):
-  H = [0, 0]
-  T = [0, 0]
-  visited = [tuple(T)]
-
-  for direction, distance in instructions:
-    for _ in range(distance):
-      if direction == "U":
-        H[1] += 1
-      elif direction == "D":
-        H[1] -= 1
-      elif direction == "L":
-        H[0] -= 1
-      elif direction == "R":
-        H[0] += 1
-      else:
-        raise Exception(f"{direction} is not a valid direction!")
-
-      move_part(H, T, direction)
-
-      if tuple(T) not in visited:
-        visited.append(tuple(T))
-
-  return len(visited)
-
-def print_rope(rope):
-  max_x = 10
-  max_y = 10
-
-  for position in rope:
-    if position[0] > max_x:
-      max_x = position[0]
-
-    if position[1] > max_y:
-      max_y = position[1]
-
-  grid = [["." for x in range(max_x)] for y in range(max_y)]
-
-  # print(grid)
-  for i, position in enumerate(reversed(rope)):
-    grid[position[1]][position[0]] = "H" if i == len(rope) - 1 else str(len(rope) - 1 - i)
-
-  for row in reversed(grid):
-    print("".join(row))
-
-  print("\n")
-
-def part_2_solution(instructions):
-  rope = [[0, 0] for _ in range(10)]
-  visited = [tuple(rope[-1])]
+def solution(instructions, rope_size=2):
+  rope = [[0, 0] for _ in range(rope_size)]
+  visited = []
 
   for direction, distance in instructions:
     for _ in range(distance):
@@ -97,33 +33,24 @@ def part_2_solution(instructions):
       else:
         raise Exception(f"{direction} is not a valid direction!")
 
-      new_rope = [rope[0]]
-
-      for i, part in enumerate(rope):
+      for i, section in enumerate(rope):
         if i == 0:
           continue
 
-        new_rope.append(move_long_part(rope[i - 1], part))
+        prev_section = rope[i - 1]
+        move(prev_section, section)
 
-      rope = new_rope
-      print_rope(rope)
-      # breakpoint()
+      if tuple(section) not in visited:
+        visited.append(tuple(section))
 
-      if tuple(rope[-1]) not in visited:
-        visited.append(tuple(rope[-1]))
-
+  # print(visited)
   return len(visited)
 
+def part_1_solution(instructions):
+  return solution(instructions)
+
+def part_2_solution(instructions):
+  return solution(instructions, 10)
 
 def transform_prompt():
-  test_data = '''R 4
-U 4
-L 3
-D 1
-R 4
-D 1
-L 5
-R 2'''.split("\n")
-
-  return [[part if i == 0 else int(part) for i, part in enumerate(line.split(" "))] for line in test_data]
-  # return [[part if i == 0 else int(part) for i, part in enumerate(line.split(" "))] for line in Prompt.read_to_list(__file__)]
+  return [[part if i == 0 else int(part) for i, part in enumerate(line.split(" "))] for line in Prompt.read_to_list(__file__)]
