8e705104a578c94a8ba1440696ef161712bf4685
Tyler Porter <tyler.b.porter@gmail.com>
2022-12-14 01:43:52 -0500

    General solution to Day 14

--- src/14/solution.py
+++ src/14/solution.py
@@ -19,31 +19,10 @@ def points_between(p1, p2):
     direction = -1 if distance[1] > 0 else 1
     return [(p1[0], p1[1] - distance[1] - (i * direction)) for i in range(abs(distance[1]) + 1)]
 
-def print_grid(grid_dict):
-  min_x = min(grid_dict, key=lambda x: x[0])[0]
-  min_y = min(grid_dict, key=lambda x: x[1])[1]
-
-  max_x = max(grid_dict, key=lambda x: x[0])[0]
-  max_y = max(grid_dict, key=lambda x: x[1])[1]
-
-  for y in range(min_y, max_y + 1):
-    row = ""
-    for x in range(min_x, max_x + 1):
-      if (x, y) in grid_dict and grid_dict[x, y] == WALL:
-        row += '#'
-      elif (x, y) in grid_dict and grid_dict[x, y] == SAND:
-        row += 'o'
-      else:
-        row += '.'
-
-    print(row)
-
 def falling_sand(lines, floor_offset=0):
   cave = {}
   floor = None
   solid_floor = floor_offset != 0
-
-  # breakpoint()
 
   for line in lines:
     prv_point = line[0]
@@ -60,8 +39,6 @@ def falling_sand(lines, floor_offset=0):
   
   floor += floor_offset
 
-  # breakpoint()
-
   grid_full = False
   while not grid_full:
     stopped = False
@@ -71,30 +48,20 @@ def falling_sand(lines, floor_offset=0):
       left_diagonal_move = tuple([grain[0] - 1, grain[1] + 1])
       right_diagonal_move = tuple([grain[0] + 1, grain[1] + 1])
 
-      # if grain[1] >= 11:
-      #   breakpoint()
-      if downward_move not in cave:
-        # Has hit the solid floor
-        if (solid_floor and downward_move[1] <= floor - 1):
-          if left_diagonal_move not in cave:
-            grain = list(left_diagonal_move)
-          elif right_diagonal_move not in cave:
-            grain = list(right_diagonal_move)
-          else:
-            cave[tuple(grain)] = SAND
-            stopped = True
-        # Has fallen off the bottom
-        elif downward_move[1] > floor:
-          grid_full = True
+      # Always make sure there's a floor under candidate moves if solid floor
+      if solid_floor:
+        cave[(downward_move[0], floor)]       = WALL
+        cave[(left_diagonal_move[0], floor)]  = WALL
+        cave[(right_diagonal_move[0], floor)] = WALL
 
-          break
-        else:
+      if not solid_floor and downward_move[1] > floor:
+        grid_full = True
+
+        break
+
+      if not solid_floor or (solid_floor and downward_move[1] <= floor):
+        if downward_move not in cave:
           grain = list(downward_move)
-      else:
-        if grain == [START_X, START_Y]:
-          grid_full = True
-          
-          break
         elif left_diagonal_move not in cave:
           grain = list(left_diagonal_move)
         elif right_diagonal_move not in cave:
@@ -103,24 +70,26 @@ def falling_sand(lines, floor_offset=0):
           cave[tuple(grain)] = SAND
           stopped = True
 
-    # print(list(cave.values()).count(SAND))
-    # print_grid(cave)
-    
+        if grain == [START_X, START_Y]:
+          grid_full = True
+          
+          break
+      else:
+        cave[tuple(grain)] = SAND
+        stopped = True
+
+        if grain == [START_X, START_Y]:
+          grid_full = True
+          
+          break
+
   return list(cave.values()).count(SAND)
 
 def part_1_solution(lines):
-  # return
   return falling_sand(lines)
 
 def part_2_solution(lines):
-  return
   return falling_sand(lines, 2)
 
-TEST = [
-  '498,4 -> 498,6 -> 496,6',
-  '503,4 -> 502,4 -> 502,9 -> 494,9'
-]
-
 def transform_prompt():
-  # return [[(int(part.split(",")[0]), int(part.split(",")[1])) for part in line.split(" -> ")] for line in TEST]
   return [[(int(part.split(",")[0]), int(part.split(",")[1])) for part in line.split(" -> ")] for line in Prompt.read_to_list(__file__)]
